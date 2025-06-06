d2bf750bd4f2bc62a26a09fa8cef93e411545f9c
Sudhanshu Chaurasia <96690394+sudha-01@users.noreply.github.com>
2025-06-06 06:31:54 +0000

    for loops

--- 05_iterations/forloop.js
+++ 05_iterations/forloop.js
@@ -13,6 +13,34 @@ for (let index = 0; index < array.length; index++) {
 for( let i =0; i<= 5; i++){
     console.log(`Outer value ${i}`)
     for(let j = 0; j <= 5; j++){
-        console.log(`Inner value ${j}`)
+        console.log(`Inner value ${j} and outer value ${i}`);
     }
+} // visualising nested loops value changes
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`Multiplication table for ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} * ${j} = ${i * j}`);
+    }
+} // multiplication tables form 1 to 10
+
+
+
+// for loop with break and continue
+
+for (let index = 1; index <= 20; index++) {
+    if(index === 5){
+        console.log(`Detected 5, breaking the loop`)
+        break; // exits the loop when index is 5
+    }
+    console.log(`Value of i is ${index}`)
 }
+
+for (let index = 1; index <= 20; index++) {
+    if(index === 5){
+        console.log(`Detected 5, skipping this iteration`);
+        continue; // skips the current iteration when index is 5
+    }
+    console.log(`Value of i is ${index}`);
+}
+
