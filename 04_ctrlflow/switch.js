61ad84e97a7540d563dfe2a6ab8384cbda9374f8
Sudhanshu Chaurasia <96690394+sudha-01@users.noreply.github.com>
2025-06-06 05:07:55 +0000

    switch case

--- 04_ctrlflow/switch.js
+++ 04_ctrlflow/switch.js
@@ -1,1 +1,30 @@
-//hi
+//switch case statement example
+
+const day = "Sunday"
+
+switch (day) {
+    case "Monday":
+        console.log("Start of the work week");
+        break;
+    case "Tuesday":
+        console.log("Second day of the work week");
+        break;
+    case "Wednesday":
+        console.log("Midweek day");
+        break;
+    case "Thursday":
+        console.log("Almost the weekend");
+        break;
+    case "Friday":
+        console.log("Last day of the work week");
+        break;
+    case "Saturday":
+        console.log("Weekend begins");
+        break;
+    case "Sunday":
+        console.log("Rest day before the new week");
+        break;
+    default:
+        console.log("Not a valid day");
+        break;
+}
